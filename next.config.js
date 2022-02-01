5df1e1396d1f69c45c5c8222af6b1014ad38b33a
JhosdryG <53978018+JhosdryG@users.noreply.github.com>
2022-02-01 06:58:38 -0400

    configure eslint and prettier with typescript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    deviceSizes: [
+      260, 300, 350, 400, 450, 500, 640, 750, 828, 1080, 1200, 1920, 2048, 3840,
+    ],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256],
+    formats: ["image/avif", "image/webp"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { NextPage } from "next";
+import React from "react";
+
+const index: NextPage = () => <h1>Hola mundo aqui empieza todo</h1>;
+export default index;
